92b62095535668aef944845f2908a187d3d64b0e
Jonathas dos Santos Pereira <72317775+jonhsp@users.noreply.github.com>
2025-06-02 15:34:56 -0300

    Update Trabalho_1.r

--- Trabalho_1.r
+++ Trabalho_1.r
@@ -5,8 +5,7 @@ library(ggplot2)
 library(corrplot)
 library(GGally)
 library(car)
-
-
+library(magrittr)
 
 # 1) Leitura dos dados
 
@@ -39,17 +38,16 @@ leitura <- function(path, names){
 }
 
 # 1.1) Índice de envelecimento
-
 path <-  "https://gist.githubusercontent.com/jonhsp/1e3e549494bfa1efb8fa25467f5bc0c7/raw/c87f3d9cd017d68a1456e352216faa8a6f90492c/%25C3%258Dndice%2520de%2520Envelhecimento"
 df_IndiceEnvelhecimento <- leitura(path, c("municipio", "regiao", "indiceEnvelhecimento"))
 
 # 1.2) População Estimada
 path <- "https://gist.githubusercontent.com/jonhsp/b2884ca9770d2cbbdecad217f19897f0/raw/c6f2b1665065190b03abd8ce86d41e3f0e92e414/Popula%25C3%25A7%25C3%25A3o%2520Estimada.txt"
-df_populacao <- leitura(path, c("municipio", "regiao", "populacaoEstimada"))
+df_populacao <- leitura(path, c("municipio", "regiao", "populacao"))
 
 # 1.3) Grau de Urbanização
 path <- "https://gist.githubusercontent.com/jonhsp/7c147642de87fe433ce086ceabb470a3/raw/c86f93fabe5a583bd701b7db3d298eab7fc1492c/Grau%2520de%2520Urbaniza%25C3%25A7%25C3%25A3o.txt"
-df_urbanizacao <- leitura(path, c("municipio", "regiao", "graudeDeUrbanizacao"))
+df_urbanizacao <- leitura(path, c("municipio", "regiao", "graudeU"))
 
 # 1.4) Despesas do Município
 path <- "https://gist.githubusercontent.com/jonhsp/d9b55c8645144be9298525417a29b9c9/raw/60dfde981f267bf285f3b218a70fede129d1a186/Despesas%2520Municipio"
@@ -57,7 +55,7 @@ df_despesas <- leitura(path, c("municipio", "regiao", "despesas"))
 
 # 1.5) Cultura
 path <- "https://gist.githubusercontent.com/jonhsp/0392551ee43bc64b3c9a1430c653bd9e/raw/747eda439e121900c44e50fe1ed9c48b5196b69d/Equipamentos%2520Culturais"
-df_cultura <- leitura(path, c("municipio", "regiao", "equipamentosCulturais"))
+df_cultura <- leitura(path, c("municipio", "regiao", "equipamentosC"))
 
 # 1.6) Escolas
 path <- "https://gist.githubusercontent.com/jonhsp/e0142669e1c9f92e8b1b07cdf48614ac/raw/926740925238633aebcbb4a4831b16fe8254b2da/Estabelecimentos"
@@ -67,18 +65,47 @@ df_escolas <- leitura(path, c("municipio", "regiao", "escolas"))
 path <- "https://gist.githubusercontent.com/jonhsp/af72cb3e56b9c2aad161f528e157c742/raw/e97cf5e93dbf1db3008029a9a3ca4548b20bfd77/Matr%25C3%25ADculas"
 df_matriculas <- leitura(path, c("municipio", "regiao", "matriculas"))
 
-# Junção dos dados
-dfs <- list(df_IndiceEnvelhecimento, df_populacao, df_urbanizacao, df_despesas, df_cultura, df_escolas, df_matriculas)
+# 1.8) Profissionais de Saude
+path <- "https://gist.githubusercontent.com/jonhsp/fdaf04aa3f0962d50c54e3ccd9bee5bb/raw/96a9644ad4bc843e52831987b39c08645bf1cf4d/Profissionais%2520de%2520Saude"
+df_profissionaisSaude <- leitura(path, c("municipio", "regiao", "profissionaisS"))
+
+
+# 2) Junção dos dados
+dfs <- list(df_IndiceEnvelhecimento,
+            df_populacao,
+            df_urbanizacao,
+            df_despesas,
+            df_cultura,
+            df_escolas,
+            df_matriculas,
+            df_profissionaisSaude)
+
 df <- Reduce(function(x, y) merge(x, y, by = c("municipio", "regiao")), dfs)
-df$logPopulacaoEstimada <- log(df$populacaoEstimada, base = exp(1))
-df$despesasPorMHabitantes <- (df$despesas / df$populacaoEstimada)*1000
-df$equipamentosCulturaisPorMHabitantes <- (df$equipamentosCulturais / df$populacaoEstimada)*1000
-df$escolasPorMHabitantes <- (df$escolas / df$populacaoEstimada)*1000
-df$matriculasPorMHabitantes <- (df$matriculas / df$populacaoEstimada)*1000
 
-# Correlação
+# 3) trasformações 
+
+df <- df %>%
+    # Log da população
+    mutate(logPopulacao = log(df_populacao$populacao)) %>%  
+    relocate(logPopulacao, .after = 4) %>%
+    # Ponderação das variáveis por mil habitantes
+    mutate(across(c("despesas", "equipamentosC", "escolas", "matriculas", "profissionaisS"),
+                ~ (.x / df$populacao) * 1000)) %>%
+    # Transformação de múnicipio e região em nome das linhas
+    unite("municipio_regiao", c("municipio", "regiao"), sep = "/") %>% 
+    column_to_rownames("municipio_regiao")
+
+
+# 4) Correlações
+
 x11("Correlação e Distribuições")
-ggpairs(df[, -c(1,2)])
+ggpairs(df)
 
 
+# 5) Ajuste do Modelo de Regressão Múltipla
+
+ajuste <- lm(indiceEnvelhecimento ~ . - populacao, df)
+summary(ajuste)
+anova(ajuste)
+Anova(ajuste)
 
